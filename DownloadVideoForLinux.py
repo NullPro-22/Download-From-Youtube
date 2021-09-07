58762c348bb341c62693dc59ba8ba1ff254686d4
Null <69919401+NullPro-22@users.noreply.github.com>
2021-09-07 18:57:01 +0300

    Add Quality option

--- DownloadVideoForLinux.py
+++ DownloadVideoForLinux.py
@@ -2,11 +2,20 @@ from pafy import new
 from os import system
 system("clear")
 video = input("Pleas Enter video url : ")
-print("The Video is Downloading...")
-print("")
 v = new(video)
-stream = v.streams
+
+stream = v.videostreams
+
+print("")
+print("Choose the desired Quality")
+print("")
+
+num = 0
 for i in stream:
-	print(i)
-	stream[1].download()
-print("The Video is Download : ")
+	print(num , "- " , i)
+	num +=1
+
+streamnum = input("")
+print("The Video is Downloading...")
+stream[int(streamnum)].download()
+print("The file has been Downloaded : ")
