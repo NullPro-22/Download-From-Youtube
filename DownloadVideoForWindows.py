0198856ac37587c892e45182760f73c2b8bd47d4
Null <69919401+NullPro-22@users.noreply.github.com>
2021-09-07 18:54:47 +0300

    Add Choose Quality option

--- DownloadVideoForWindows.py
+++ DownloadVideoForWindows.py
@@ -1,12 +1,22 @@
 from pafy import new
 from os import system
+
 system("cls")
 video = input("Pleas Enter video url : ")
-print("The Video is Downloading...")
-print("")
 v = new(video)
-stream = v.streams
+
+stream = v.videostreams
+
+print("")
+print("Choose the desired Quality")
+print("")
+
+num = 0
 for i in stream:
-	print(i)
-	stream[0].download()
-print("The Video is Download : ")
+	print(num , "- " , i)
+	num +=1
+
+streamnum = input("")
+print("The Video is Downloading...")
+stream[int(streamnum)].download()
+print("The file has been Downloaded : ")
