2930b02c5282c4981f438efb30cb7f0cdaaffd71
marwa21-k <koroko123430@gmail.com>
2021-09-07 19:44:25 +0300

    Download Video or Audio

---
+++ DownloadVideoOrAudioForWindows.py
@@ -0,0 +1,26 @@
+from pafy import new
+from os import system
+
+system("cls")
+video = input("Pleas Enter video url : ")
+v = new(video)
+type = input("For video [1] for audio [2]: ")
+stream = None
+
+if (int(type) == 1):
+	stream = v.videostreams
+else:
+	stream = v.m4astreams
+
+print("")
+print("Choose the desired Quality")
+print("")
+
+num = 0
+for i in stream:
+	print(num , "- " , i)
+	num +=1
+
+streamnum = input("")
+stream[int(streamnum)].download()
+print("The file has been Downloaded : ")
